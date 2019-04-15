2d1b65fce92ed0489ef4bc52dcfc60fd2078993f
Михаил Березин <Mihail14112000@mail.ru>
2019-04-15 23:07:58 +0300

    1

--- DZ1/BaseDepartment.h
+++ DZ1/BaseDepartment.h
@@ -4,16 +4,16 @@
 
 class BaseDepartment : public Department {
 private:
-	
+
 	std::string SaEC;			 //Scientific and educational complex
 	std::string nameOfDepartment;
 	int numOfSubdepartments;
 	Subdepartment D;
-	
-	
+
+
 	std::vector<Subdepartment> vecSubDep; // вектор кафедр
 public:
-	
+
 	//std::vector <InfoOfSubdepartment> Subdepartments; // вектор кафедр
 
 
@@ -22,8 +22,8 @@ public:
 
 	virtual Subdepartment getVecSubDep(int index) { return (vecSubDep[index]); };
 	virtual std::string getNOD() { return nameOfDepartment; }
-	virtual int getnumOfSub() {	return numOfSubdepartments; }
-	virtual std::string getSaEC() { return SaEC;  }
+	virtual int getnumOfSub() { return numOfSubdepartments; }
+	virtual std::string getSaEC() { return SaEC; }
 	virtual void readFromFile(/*std::string*/);
 	virtual void WriteToFile(/*std::string*/);
 	virtual int calcNumOfSubjects();
